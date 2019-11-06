b0c1dd708179c92745d372c55dc1ce21d154774f
1vlada1 <1vladastojanovic1@gmail.com>
2019-11-06 23:57:46 +0100

    Set initialExpenses and inital app view

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import ExpenseForm from './components/ExpenseForm';
 import ExpenseList from './components/ExpenseList'
 import Alert from './components/Alert'
+import uuid from 'uuid/v4'
+
+const initialExpenses = [
+  { id: uuid(), charge: 'rent', amount: 1600 },
+  { id: uuid(), charge: 'car payment', amount: 400 },
+  { id: uuid(), charge: 'credit card bill', amount: 1200 }
+];
+
 
 function App() {
+
+  const [expenses, setExpenses] = useState(initialExpenses);
+
   return (
     <>
       <Alert />
-      <ExpenseForm />
-      <ExpenseList />
+      <h1>budget calculator</h1>
+      <main className='App'>
+        <ExpenseForm />
+        <ExpenseList expenses={expenses} />
+      </main>
+      <h1>total spending : <span className='total'>
+        $ {expenses.reduce((acc, curr) => {
+          return (acc += curr.amount)
+        }, 0)}
+      </span></h1>
     </>
   )
 }
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import Item from './ExpenseItem';
+import { MdDelete } from 'react-icons/md'
 
-
-const ExpenseList = () => {
+const ExpenseList = ({ expenses }) => {
     return (
-        <div>
-            hell from expense list
-            <Item />
-        </div>
+        <>
+            <ul className='list'>
+                {expenses.map((expense) => {
+                    return <Item key={expense.id} expense={expense} />;
+                })}
+            </ul>
+            {expenses.length > 0 && <button className='btn'>clear expenses
+            <MdDelete className='btn-icon' />
+            </button>}
+        </>
     )
 }
 
