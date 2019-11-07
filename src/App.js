0609c0df93d5bd03b479b286d8c56d04d8b5a508
1vlada1 <1vladastojanovic1@gmail.com>
2019-11-07 15:37:55 +0100

    Show success and danger alert

--- src/App.js
+++ src/App.js
@@ -20,13 +20,25 @@ function App() {
   const [charge, setCharge] = useState('')
   // single amount
   const [amount, setAmount] = useState('')
+  // Alert
+  const [alert, setAlert] = useState({ show: false })
   // ********************** functionality ********************
+  // Handle Charge
   const handleCharge = e => {
     setCharge(e.target.value)
   }
+  // Handle Amount
   const handleAmount = e => {
     setAmount(e.target.value)
   }
+  // Handle Alert
+  const handleAlert = ({ type, text }) => {
+    setAlert({ show: true, type, text })
+    setTimeout(() => {
+      setAlert({ show: false })
+    }, 5000)
+  }
+
   const handleSubmit = e => {
     e.preventDefault()
 
@@ -37,17 +49,22 @@ function App() {
         amount: amount
       }
       setExpenses([...expenses, singleExpense])
+      handleAlert({ type: 'success', text: 'item added' })
       setCharge('')
       setAmount('')
     } else {
       // handle alert called
+      handleAlert({
+        type: 'danger',
+        text: `charge can't be empty value and amount value has to be bigger thn zero `
+      })
     }
   }
 
 
-
   return (
     <>
+      {alert.show && <Alert type={alert.type} text={alert.text} />}
       <Alert />
       <h1>budget calculator</h1>
       <main className='App'>
